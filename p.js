24e52a4cbddfd8a9374e087233befc1ea66edf11
Anally3 <126262067+Anally3@users.noreply.github.com>
2024-08-26 08:52:36 -0300

    Create p.js

---
+++ p.js
@@ -0,0 +1,46 @@
+export const perguntas = [
+    {
+        enunciado: "Na aula de química seu professor aborda sobre um tema desconhecido, a sustenta bilidade",
+        alternativas: [
+            
+            {
+                texto: "eu nunca ouvi falar sobre",
+                afirmacao: [
+                    "Ficou com várias duvidas.",
+                    "Pensou em várias possibilidades do que pode ser."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "eu conheço!",
+                afirmacao: [
+                    "Quer saber como se aplica no dia dia.",
+                    "Pensou nas formas de utiliza-la."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Ao dar o sinal para começar, você tira as suas dúvidas com o profesor",
+        alternativas: [
+            {
+                texto: "é o conceito de viver e operar de maneira que respeite e preserve os recursos naturais e sociais para garantir um futuro saudável e equitativo!",
+                afirmacao: [
+                    "Produz vários benefícios.",
+                    "Percebe que ajuda muito o planeta.",
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Nossa isso parece ser muito bom mesmo!",
+                afirmacao: [
+                    "Sim! Ela assegura condições de vida dignas para nós e o nosso futuro, apoiando os direitos humanos!",
+                    
+                    
+                ],
+            },
+        ]
+    }
+];
